0a4272448d6975ff47c9564b09a370f0f2a7ad62
Dan White <44091159+dandancheeseandham@users.noreply.github.com>
2023-12-18 15:33:52 +0000

    Create sort out whitelisting domains.ps1

---
+++ General/sort out whitelisting domains.ps1
@@ -0,0 +1,34 @@
+﻿# The input string
+$inputString = "example, name <name@example.com>; Othername <anothername@otherexample.com>; "
+
+# Common and shared email domains
+$commonDomains = @('hotmail.com', 'outlook.com', 'gmail.com')
+
+# Split the string into individual entries
+$entries = $inputString -split ';'
+
+# Create a hashtable to hold the unique results
+$uniqueResults = @{}
+
+foreach ($entry in $entries) {
+    # Trim white spaces
+    $entry = $entry.Trim()
+
+    # Skip empty entries
+    if (-not $entry) { continue }
+
+    # Extract the email address
+    $email = if ($entry -match '<(.*?)>') { $matches[1] } else { $entry -replace '.* ', '' }
+
+    # Extract the domain from the email
+    $domain = $email -replace '.*@', ''
+
+    # Determine the output (full email or just domain)
+    $output = if ($domain -in $commonDomains) { $email } else { $domain }
+
+    # Add to the hashtable if not already present
+    $uniqueResults[$output] = $true
+}
+
+# Output the results to a file
+$uniqueResults.Keys | Out-File -FilePath "unique_domains.txt"
